8ee1729ddb6c0d04c1f734d7577846a64d024d89
good593 <goodwon593@gmail.com>
2025-08-28 20:38:01 +0900

    작업중

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ warnings.filterwarnings('ignore')
 import logging
 logging.basicConfig(level=logging.INFO)
 
+import pandas as pd 
+
 from common.dataset import load_dataset
 from common.preprocessing.eda import do_eda
 from common.preprocessing.cleaning import do_cleaning
@@ -13,8 +15,8 @@ def modeling():
     logging.info("##################################")
     logging.info("Start Load Dataset")
     # 1. train dataset, test dataset 
-    df_train = load_dataset("./data/train.csv")
-    df_test = load_dataset("./data/test.csv")
+    df_train, df_train_target = load_dataset("./data/train.csv", target_col="survived")
+    df_test, _ = load_dataset("./data/test.csv")
     # 2. eda
     do_eda(df_train=df_train)
     # 3. cleaning
@@ -22,7 +24,16 @@ def modeling():
 
     # 4. encoding
     enc_cols = ['gender', 'embarked']
+    normal_cols = list(set(df_train.columns) - set(enc_cols))
     tr_enc, te_enc = do_encoding(df_train[enc_cols], df_test[enc_cols])
+    train = pd.concat(
+        [df_train[normal_cols].reset_index(drop=True), tr_enc.reset_index(drop=True)]
+        , axis=1
+    )
+    test = pd.concat(
+        [df_test[normal_cols].reset_index(drop=True), te_enc.reset_index(drop=True)]
+        , axis=1
+    )
 
     # 5. create model
 
